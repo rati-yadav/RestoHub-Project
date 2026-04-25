add4a0200b4e18981c85a0ae8116ba9020562a1a
rati yadav <ratiyadav968@gmail.com>
2026-04-25 11:59:26 +0530

    removed live_chat context processor

--- Resturant_Project/Resturant_Project/settings.py
+++ Resturant_Project/Resturant_Project/settings.py
@@ -74,7 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'Base_App.context_processors.live_chat',
+                # 'Base_App.context_processors.live_chat',
             ],
         },
     },
